00710fcd6940da653467fc4e498b5df91f44427c
BryanC-975 <83613049+BryanC975@users.noreply.github.com>
2022-12-27 19:25:45 +0100

    Add files via upload

---
+++ python_basic/Tipos_var.py
@@ -0,0 +1,21 @@
+#Variables#
+
+string = "Frase variable"
+print(string)
+print('---------------')
+
+int = 5
+print(int)
+print('---------------')
+
+booleano = "False"
+print(booleano)
+print('---------------')
+
+
+# Concatenacion #
+
+print(string,'|', int,'|', booleano)
+print('---------------')
+print("Esta es el contenido de string:", string )
+
