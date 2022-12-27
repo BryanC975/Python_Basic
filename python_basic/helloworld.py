d8c9c44531376ba140036814cc7a63373ce288fe
BryanC-975 <83613049+BryanC975@users.noreply.github.com>
2022-12-27 16:07:43 +0100

    Add files via upload

---
+++ python_basic/helloworld.py
@@ -0,0 +1,27 @@
+## Hola mundo ##
+
+print("Hola mundo")
+print ('---------------------')
+## Diferentes tipos de datos ##
+
+var = 5;
+print (var)
+print (type(var))
+print ('----------')
+var = "Hola";
+print (var)
+print (type(var))
+print ('----------')
+var = 1,5;
+print (var)
+print (type(var))
+print ('----------')
+var = 8 + 1j;
+print (var)
+print (type(var))
+print ('----------')
+var = True;
+print (var)
+print (type(var))
+print ('----------')
+print(type(print("Mi cadena de texto"))) 
